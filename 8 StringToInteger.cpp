c0301ec3e55e8bed8fa75c0152327b4e41b47d5c
qCuteArthur <2217594536@qq.com>
2018-05-08 22:37:51 +0800

    Init

---
+++ 8 StringToInteger.cpp
@@ -0,0 +1,11 @@
+#include<iostream> 
+using namespace std;
+int myAtoi(string str);
+int main(){
+	int ret = myAtoi("hello world,justin");
+	return 0;
+}
+int myAtoi(string str){
+	printf("hello world!");
+	printf("______");
+}
